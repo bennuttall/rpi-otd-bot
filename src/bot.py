cfc71ce3401d282978eb601eb2bc2abaea55a1ba
Ben Nuttall <ben@bennuttall.com>
2022-10-15 12:04:14 +0100

    Add logging when retrieving posts

--- src/bot.py
+++ src/bot.py
@@ -24,7 +24,9 @@ month = now.date().month
 day = now.date().day
 year = 2002 + now.time().hour  # 9am = 2011
 post_date = date(year, month, day)
+logger.info("Getting posts", date=post_date)
 posts = db.get_posts_by_date(post_date)
+logger.info("Number of posts found", n=len(posts))
 
 for post in posts:
     title = html.unescape(post['title'])
